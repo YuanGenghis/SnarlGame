e982f48f0ef4e8d885d8b1f16e23a6bffa6989b4
hollis <hong.jiah@husky.neu.edu>
2021-03-11 12:17:47 -0500

    comment GameManager

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,20 +1,24 @@
 import java.util.List;
 
+// represents the GameManager
 public class GameManager {
   List<Player> players;
   Level level;
   Player curPlayer;
 
+  // construct the GameManager with given parameters
   public GameManager(List<Player> players, Level level, Player curPlayer) {
     this.players = players;
     this.level = level;
     this.curPlayer = curPlayer;
   }
 
+  // init the level?
   public void init(Level level) {
     this.level = level;
   }
 
+  // test if the name is valid
   boolean isValidName(List<Player> players, String name) {
     for (Player p: players) {
       if (p.name.equals(name)) {
