c96806d86385e76098acd9ce84e47de7b33120b6
hollis <hong.jiah@husky.neu.edu>
2021-03-11 12:13:11 -0500

    add comments to Hallway

--- src/Hallway.java
+++ src/Hallway.java
@@ -3,11 +3,13 @@ import java.util.List;
 
 import javafx.util.Pair;
 
+// represents a Hallway
 public class Hallway {
   List<Pair<Integer,Integer>> layout;
   Pair<Pair<Integer, Integer>, Pair<Integer, Integer>> connection;
   List<Pair<Integer, Integer>> waypoints;
 
+  // construct the Hallway
   public Hallway(List<Pair<Integer,Integer>> tilesPosition,
                  Pair<Pair<Integer, Integer>, Pair<Integer, Integer>> connection,
                  List<Pair<Integer, Integer>> waypoints) {
@@ -16,22 +18,25 @@ public class Hallway {
     this.waypoints = waypoints;
   }
 
+  // init a default Hallway
   public Hallway() {
     this.layout = new ArrayList<>();
     this.connection = new Pair<>(new Pair<>(0,0), new Pair<>(0,0));
     this.waypoints = new ArrayList<>();
   }
 
+  // set the connection of the Hallway
   public void setConnection(int x1, int y1, int x2, int y2) {
     this.connection = new Pair<>(new Pair<>(x1,y1), new Pair<>(x2,y2));
   }
 
+  // set the waypoints of the Hallway
   public void setWaypoints(List<Pair<Integer, Integer>> ways) {
     this.waypoints = ways;
   }
 
+  // set the layout of the Hallway
   public void setLayout(List<Pair<Integer,Integer>> layout) {
     this.layout = layout;
   }
-
 }
