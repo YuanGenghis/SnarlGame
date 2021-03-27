1e5c0feae587e15d31aacd69f50612159b644014
hollis <hong.jiah@husky.neu.edu>
2021-03-27 11:52:37 -0400

    added UserInterface

--- src/User.java
+++ src/User.java
@@ -1,4 +1,46 @@
-public interface User {
-  public void update(int[] position, Level level);
-}
+import java.awt.event.KeyEvent;
+import java.util.*;
 
+public class User implements UserInterface{
+    GameManager gm;
+
+
+    public User(List<String> names) {
+        this.gm = new GameManager(names);
+        gm.init();
+    }
+
+    // deal with arrow movement of user
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        switch( keyCode ) {
+            case KeyEvent.VK_UP:
+                // handle up
+                System.out.println(1);
+                break;
+            case KeyEvent.VK_DOWN:
+                // handle down
+                break;
+            case KeyEvent.VK_LEFT:
+                // handle left
+                break;
+            case KeyEvent.VK_RIGHT :
+                // handle right
+                break;
+        }
+    }
+    public static void main(String[] args) {
+        User user = new User(Arrays.asList("JC", "hollis"));
+//        Timer t = new Timer();
+//        t.scheduleAtFixedRate(
+//                new TimerTask() {
+//                    @Override
+//                    public void run() {
+//                        keyPressed();
+//                    }
+//                }, 0, 1000
+//        );
+
+    }
+
+}
---
+++ src/UserInterface.java
@@ -0,0 +1,6 @@
+import javafx.util.Pair;
+
+public interface UserInterface {
+
+    //public void update(int[] position, Level level);
+}
